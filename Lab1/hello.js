16bd35095c31b9f33c075feac543a3a2c1029e8c
Adarsh Singh gaur <adarshsinghgaur2007@gmail.com>
2026-07-21 14:47:38 +0530

    add function

---
+++ Lab1/hello.js
@@ -0,0 +1,16 @@
+function sayHello(name) {
+    console.log("Welcome to " + name + "!");
+}
+const sayHi = (name) => {
+    console.log("Hi " + name + "!");
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+
+
+sayHello('Adarsh Singh');
+sayHi('Adarsh Singh');
+console.log("Sum of 5 and 10 is: " + sum(5, 10));
