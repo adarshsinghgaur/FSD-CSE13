0e9dc2a541b580982149c4a9ffa9566146a20912
Adarsh Singh gaur <adarshsinghgaur2007@gmail.com>
2026-07-28 13:28:16 +0530

    event

--- Lab1/event.js
+++ Lab1/event.js
@@ -1,10 +1,16 @@
 const f1 = () => {
-    log('F1');
-
-}
+  log("F1");
+};
 const f2 = () => {
-    log("F2");
-}
+  log("F2");
+};
 const f3 = () => {
-    log("F3")
-}
+  log("F3");
+};
+const main = () => {
+  log("main");
+  f1();
+  f2();
+  f3();
+  log("end");
+};
